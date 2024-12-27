cf10ed51a5981b6d2b65e9f51e1750d755d44d0d
Yashavantha P <yashavanthap.btech23@gmail.com>
2024-12-27 12:12:32 +0530

    updated

--- backend/dataBase/Sql.js
+++ backend/dataBase/Sql.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'y18a07s25hu',
-  database: 'hospital_db',
+  host:process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
